c7d0ae3c5721c15489e73353d938a1746678eeb3
Hannah Adnan Abdulrehman <hannahabdulrehman@gmail.com>
2023-02-07 14:54:32 +0000

    changes to salmon website

--- app.js
+++ app.js
@@ -140,3 +140,33 @@ CookieStore.prototype.intro = function () {
 // }
 
 // document.body.appendChild(tableEl);
+
+
+
+
+
+
+//for the new store form
+
+
+const form = document.getElementById("new-store-form");
+
+form.addEventListener("submit") function(event) {
+  event.preventDefault();
+
+  const storeNameInput = event.target.name.value;
+  const minCustInput = event.target.minCust.value;
+  const maxCustInput = event.target.maxCust.value;
+  const avgCookieInput = event.target.avgCookies.value;
+
+  console.log(storeNameInput);
+  console.log(minCustInput);
+  console.log(maxCustInput);
+  console.log(avgCookieInput);
+
+  form.reset();
+
+  const newStore = new CookieStore(storeNameInput, minCustInput, maxCustInput, avgCookieInput);
+
+  newStore.render();
+};
